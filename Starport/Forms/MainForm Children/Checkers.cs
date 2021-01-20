83f583379bfcc340520710880f00ec961d62220d
roku674 <af03848@georgiasouthern.edu>
2021-01-19 23:28:45 -0500

    issue with Check Needs Defense
    Needs defense is duplicating the last entry still unresolved

--- Starport/Forms/MainForm Children/Checkers.cs
+++ Starport/Forms/MainForm Children/Checkers.cs
@@ -301,6 +301,10 @@ namespace StarportExcel
                                     {
                                         excel.WriteToCell(k, 11, next);
                                         Console.WriteLine("Moved " + next + " up 1");
+                                        /*if (excel.ReadCellString(k+2,11) == "")
+                                        {
+                                            excel.WriteToCell(k + 2, 11, "");
+                                        }*/
                                     }
                                 }//for k                               
                             }
