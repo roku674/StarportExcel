8e34aaa4ffca656e2ededb7803c2276c281690fb
roku674 <af03848@georgiasouthern.edu>
2021-02-12 19:41:09 -0500

    message box again

--- Starport/Forms/PlanetInfoForm.cs
+++ Starport/Forms/PlanetInfoForm.cs
@@ -18,231 +18,233 @@ namespace StarportExcel
         {
             InitializeComponent();
         }
-
         private void AddInfoButton_Click(object sender, EventArgs e)
         {
             string info = InfoBox.Text;
             if (info.Equals("") || info.Equals(null))
             {
-                MessageBox.Show("No Information Foound!", "Failed");
+                MessageBox.Show("No Information Found!", "Failed");
             }
             //Console.Write(info);
-
-            for(int i = 0; i < info.Length; i++)
+            else
             {
-                if (i + 2 < info.Length && info[i].Equals('A') && info[i + 1].Equals('r') && info[i + 2].Equals('c'))
+                for (int i = 0; i < info.Length; i++)
                 {
-                    //i+3 and i+4 are the numbers if its triple digit 3 4 5
-                    if (info[i + 5] == '.')
+                    if (i + 2 < info.Length && info[i].Equals('A') && info[i + 1].Equals('r') && info[i + 2].Equals('c'))
                     {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string s = str1 + str2;
+                        //i+3 and i+4 are the numbers if its triple digit 3 4 5
+                        if (info[i + 5] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string s = str1 + str2;
 
-                        Adder.AddInfo(2, info, int.Parse(s));
-                        break;
+                            Adder.AddInfo(2, info, int.Parse(s));
+                            break;
+                        }
+                        else if (info[i + 6] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string str3 = info[i + 5].ToString();
+                            string s = str1 + str2 + str3;
+
+                            Adder.AddInfo(2, info, int.Parse(s));
+                            break;
+                        }
+
                     }
-                    else if (info[i + 6] == '.')
+                    else if (i + 2 < info.Length && info[i].Equals('D') && info[i + 1].Equals('e') && info[i + 2].Equals('s'))
                     {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string str3 = info[i + 5].ToString();
-                        string s = str1 + str2 + str3;
 
-                        Adder.AddInfo(2, info, int.Parse(s));
-                        break;
+                        if (info[i + 5] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string s = str1 + str2;
+
+                            Adder.AddInfo(3, info, int.Parse(s));
+                            break;
+                        }
+                        else if (info[i + 6] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string str3 = info[i + 5].ToString();
+                            string s = str1 + str2 + str3;
+
+                            Adder.AddInfo(3, info, int.Parse(s));
+                            break;
+                        }
+                    }
+                    else if (i + 2 < info.Length && info[i].Equals('E') && info[i + 1].Equals('a') && info[i + 2].Equals('r'))
+                    {
+
+                        if (info[i + 5] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string s = str1 + str2;
+
+                            Adder.AddInfo(4, info, int.Parse(s));
+                            break;
+                        }
+                        else if (info[i + 6] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string str3 = info[i + 5].ToString();
+                            string s = str1 + str2 + str3;
+
+                            Adder.AddInfo(4, info, int.Parse(s));
+                            break;
+                        }
+                    }
+                    else if (i + 2 < info.Length && info[i].Equals('G') && info[i + 1].Equals('r') && info[i + 2].Equals('e'))
+                    {
+
+                        if (info[i + 5] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string s = str1 + str2;
+
+                            Adder.AddInfo(5, info, int.Parse(s));
+                            break;
+                        }
+                        else if (info[i + 6] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string str3 = info[i + 5].ToString();
+                            string s = str1 + str2 + str3;
+
+                            Adder.AddInfo(5, info, int.Parse(s));
+                            break;
+                        }
+                    }
+                    else if (i + 2 < info.Length && info[i].Equals('M') && info[i + 1].Equals('o') && info[i + 2].Equals('u'))
+                    {
+
+                        if (info[i + 5] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string s = str1 + str2;
+
+                            Adder.AddInfo(6, info, int.Parse(s));
+                            break;
+                        }
+                        else if (info[i + 6] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string str3 = info[i + 5].ToString();
+                            string s = str1 + str2 + str3;
+
+                            Adder.AddInfo(6, info, int.Parse(s));
+                            break;
+                        }
+                    }
+                    else if (i + 2 < info.Length && info[i].Equals('O') && info[i + 1].Equals('c') && info[i + 2].Equals('e'))
+                    {
+
+                        if (info[i + 5] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string s = str1 + str2;
+
+                            Adder.AddInfo(7, info, int.Parse(s));
+                            break;
+                        }
+                        else if (info[i + 6] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string str3 = info[i + 5].ToString();
+                            string s = str1 + str2 + str3;
+
+                            Adder.AddInfo(7, info, int.Parse(s));
+                            break;
+                        }
+                    }
+                    else if (i + 2 < info.Length && info[i].Equals('I') && info[i + 1].Equals('G') && info[i + 2].Equals('P'))
+                    {
+
+                        if (info[i + 5] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string s = str1 + str2;
+
+                            Adder.AddInfo(8, info, int.Parse(s));
+                            break;
+                        }
+                        else if (info[i + 6] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string str3 = info[i + 5].ToString();
+                            string s = str1 + str2 + str3;
+
+                            Adder.AddInfo(8, info, int.Parse(s));
+                            break;
+                        }
+                    }
+                    else if (i + 2 < info.Length && info[i].Equals('R') && info[i + 1].Equals('o') && info[i + 2].Equals('c'))
+                    {
+
+                        if (info[i + 5] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string s = str1 + str2;
+
+                            Adder.AddInfo(9, info, int.Parse(s));
+                            break;
+                        }
+                        else if (info[i + 6] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string str3 = info[i + 5].ToString();
+                            string s = str1 + str2 + str3;
+
+                            Adder.AddInfo(9, info, int.Parse(s));
+                            break;
+                        }
+                    }
+                    else if (i + 2 < info.Length && info[i].Equals('V') && info[i + 1].Equals('o') && info[i + 2].Equals('l'))
+                    {
+                        if (info[i + 5] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string s = str1 + str2;
+
+                            Adder.AddInfo(10, info, int.Parse(s));
+                            break;
+                        }
+                        else if (info[i + 6] == '.')
+                        {
+                            string str1 = info[i + 3].ToString();
+                            string str2 = info[i + 4].ToString();
+                            string str3 = info[i + 5].ToString();
+                            string s = str1 + str2 + str3;
+
+                            Adder.AddInfo(10, info, int.Parse(s));
+                            break;
+                        }
                     }
 
+                    //check for builds
                 }
-                else if (i + 2 < info.Length && info[i].Equals('D') && info[i + 1].Equals('e') && info[i + 2].Equals('s'))
-                {
 
-                    if (info[i + 5] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string s = str1 + str2;
-
-                        Adder.AddInfo(3, info, int.Parse(s));
-                        break;
-                    }
-                    else if (info[i + 6] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string str3 = info[i + 5].ToString();
-                        string s = str1 + str2 + str3;
-
-                        Adder.AddInfo(3, info, int.Parse(s));
-                        break;
-                    }
-                }
-                else if (i + 2 < info.Length && info[i].Equals('E') && info[i + 1].Equals('a') && info[i + 2].Equals('r'))
-                {
-
-                    if (info[i + 5] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string s = str1 + str2;
-
-                        Adder.AddInfo(4, info, int.Parse(s));
-                        break;
-                    }
-                    else if (info[i + 6] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string str3 = info[i + 5].ToString();
-                        string s = str1 + str2 + str3;
-
-                        Adder.AddInfo(4, info, int.Parse(s));
-                        break;
-                    }
-                }
-                else if (i + 2 < info.Length && info[i].Equals('G') && info[i + 1].Equals('r') && info[i + 2].Equals('e'))
-                {
-
-                    if (info[i + 5] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string s = str1 + str2;
-
-                        Adder.AddInfo(5, info, int.Parse(s));
-                        break;
-                    }
-                    else if (info[i + 6] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string str3 = info[i + 5].ToString();
-                        string s = str1 + str2 + str3;
-
-                        Adder.AddInfo(5, info, int.Parse(s));
-                        break;
-                    }
-                }
-                else if (i + 2 < info.Length && info[i].Equals('M') && info[i + 1].Equals('o') && info[i + 2].Equals('u'))
-                {
-
-                    if (info[i + 5] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string s = str1 + str2;
-
-                        Adder.AddInfo(6, info, int.Parse(s));
-                        break;
-                    }
-                    else if (info[i + 6] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string str3 = info[i + 5].ToString();
-                        string s = str1 + str2 + str3;
-
-                        Adder.AddInfo(6, info, int.Parse(s));
-                        break;
-                    }
-                }
-                else if (i + 2 < info.Length && info[i].Equals('O') && info[i + 1].Equals('c') && info[i + 2].Equals('e'))
-                {
-
-                    if (info[i + 5] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string s = str1 + str2;
-
-                        Adder.AddInfo(7, info, int.Parse(s));
-                        break;
-                    }
-                    else if (info[i + 6] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string str3 = info[i + 5].ToString();
-                        string s = str1 + str2 + str3;
-
-                        Adder.AddInfo(7, info, int.Parse(s));
-                        break;
-                    }
-                }
-                else if (i + 2 < info.Length && info[i].Equals('I') && info[i + 1].Equals('G') && info[i + 2].Equals('P'))
-                {
-
-                    if (info[i + 5] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string s = str1 + str2;
-
-                        Adder.AddInfo(8, info, int.Parse(s));
-                        break;
-                    }
-                    else if (info[i + 6] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string str3 = info[i + 5].ToString();
-                        string s = str1 + str2 + str3;
-
-                        Adder.AddInfo(8, info, int.Parse(s));
-                        break;
-                    }
-                }
-                else if (i + 2 < info.Length && info[i].Equals('R') && info[i + 1].Equals('o') && info[i + 2].Equals('c'))
-                {
-
-                    if (info[i + 5] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string s = str1 + str2;
-
-                        Adder.AddInfo(9, info, int.Parse(s));
-                        break;
-                    }
-                    else if (info[i + 6] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string str3 = info[i + 5].ToString();
-                        string s = str1 + str2 + str3;
-
-                        Adder.AddInfo(9, info, int.Parse(s));
-                        break;
-                    }
-                }
-                else if (i + 2 < info.Length && info[i].Equals('V') && info[i + 1].Equals('o') && info[i + 2].Equals('l'))
-                {
-                    if (info[i + 5] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string s = str1 + str2;
-
-                        Adder.AddInfo(10, info, int.Parse(s));
-                        break;
-                    }
-                    else if (info[i + 6] == '.')
-                    {
-                        string str1 = info[i + 3].ToString();
-                        string str2 = info[i + 4].ToString();
-                        string str3 = info[i + 5].ToString();
-                        string s = str1 + str2 + str3;
-
-                        Adder.AddInfo(10, info, int.Parse(s));
-                        break;
-                    }
-                }
-
-                //check for builds
+                MessageBox.Show("Planet Information Added!", "Completed");
+                InfoBox.Text = "";
             }
-
-            MessageBox.Show("Planet Information Added!", "Completed");
-            InfoBox.Text = "";
+            
         }
         public void SetExcelPath(string path)
         {
